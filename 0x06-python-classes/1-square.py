f1c1f507019687b3b790b548665ef688ad6d11c4
Joy Wanjiru <70502261+Joy879@users.noreply.github.com>
2021-12-09 00:32:47 +0300

    Adding Documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" Defining a class Square with a private attribute size
+""""
+
 class Square:
+"”" Defining the class Square"""
+
     def __init__(self, size):
+""" Initializing attribute size"""
         self.__size = size
