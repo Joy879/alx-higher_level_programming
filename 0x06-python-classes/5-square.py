ae29a714feb0fabb81e5adac8ff7f4b5883e5461
Joy Wanjiru <70502261+Joy879@users.noreply.github.com>
2021-12-09 03:09:46 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,14 +1,37 @@
 #!/usr/bin/python3
+""" define a class square
+"""
 class Square:
+    """Creates a class named Square
+    """
+
     def __init__(self, size=0):
+        """__init__ method intitializes an instance
+        Arg:
+            size: a private instance attribute, size of square
+        """
+
         self.__size = size
 
     @property
     def size(self):
+        """@property decorates getter function size() to access
+            private variable size value
+        Return:
+            return __size value
+        """
+
         return self.__size
 
     @size.setter
     def size(self, value):
+        """@size.setter decorates setter function size() to set
+            private variable size value
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+
         if type(value) is not int:
             raise TypeError('size must be an integer')
         if value < 0:
@@ -16,9 +39,19 @@ class Square:
         self.__size = value
 
     def area(self):
+        """public method area() caculates the area of square
+        Arg:
+            size: the size of square
+        Return:
+            return the area of square
+        """
+
         return self.size**2
 
     def my_print(self):
+        """public method my_print prints the square to the standard output
+        """
+
         if self.size > 0:
             for i in range(self.size):
                 print("#"*self.size)
