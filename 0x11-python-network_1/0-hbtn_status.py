a12a5939e462648c009785479db67c71d62fc569
Joy879 <joywanjiru879@gmail.com>
2022-03-18 18:59:15 -0700

    Python Network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ Fetches https://intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print("\t- type: {}".format(type(html)))
