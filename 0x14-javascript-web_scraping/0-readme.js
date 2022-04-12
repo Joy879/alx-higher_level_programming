61735ca1cebc88c176ade915e967d6cecf872e83
Joy879 <joywanjiru879@gmail.com>
2022-04-12 14:13:43 -0700

    Web Scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const filePath = process.argv[2];
 const fs = require('fs');
-fs.readFile(filePath,'utf8', function (err, data) {
+fs.readFile(filePath, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
